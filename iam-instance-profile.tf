d9b43869658348b9732d70858f8172fae6d476ee
Nitin Das <37865475+nitinda@users.noreply.github.com>
2020-04-15 14:33:08 +0100

    Update iam-instance-profile.tf

--- iam-instance-profile.tf
+++ iam-instance-profile.tf
@@ -1,5 +1,10 @@
 resource "aws_iam_instance_profile" "iam_instance_profile" {
-  name = var.name
-  role = var.role
-  path = var.path
+  name        = var.name
+  name_prefix = var.name_prefix
+  path        = var.path
+  role        = var.role
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 }
